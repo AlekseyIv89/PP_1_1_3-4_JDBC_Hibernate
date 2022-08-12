17f7969727a3f5e26da5e601a39b83b42b013ba1
KOMa <alekseyiv89@mail.ru>
2022-08-12 15:46:38 +0300

    create table method

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,44 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getInstance().getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    private void createDatabaseIfNotExists() {
+        String sql = "USE person;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute("CREATE DATABASE IF NOT EXISTS person;");
+            preparedStatement.execute("USE person;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void createUsersTable() {
+        createDatabaseIfNotExists();
+
+        String sql = "CREATE TABLE users (" +
+                "id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE, " +
+                "user_name VARCHAR(255) NOT NULL, " +
+                "user_lastname VARCHAR(255) NOT NULL, " +
+                "user_age TINYINT NOT NULL);";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Util instance = null;
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+    private static final String DB_NAME = "testdb";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+
+    private Util() {
+    }
+
+    public static Util getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.err.println("ERROR");
+        }
+        return connection;
+    }
 }
